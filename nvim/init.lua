86ecc230fd5f8f035d2d33cadc60aeaac1dcfb00
Nicolò Giannini <nicogiannini@yahoo.com>
2023-08-15 15:26:19 +0200

    nvim setup

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+require("default")
+
+vim.cmd("colorscheme zellner")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.encoding = "UTF-8"
+vim.opt.hlsearch = true
+vim.cmd("syntax enable")
